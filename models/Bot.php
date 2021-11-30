340c1cc6be91e767a4eee3609c29a713f4237935
vreds <slyvkinv@gmail.com>
2021-11-30 04:44:08 +0200

    Merge remote-tracking branch 'origin/master'

--- models/Bot.php
+++ models/Bot.php
@@ -553,15 +553,17 @@ class Bot extends \yii\db\ActiveRecord
 
 
 
-	public function checkCrequest($chat_id,$username){
+    /**
+     * @return bool to chat_id
+     */
+	public static function checkCrequest($chat_id,$username){
         $user = User::find()->where(['username' => $username])->one();
         $timeUserAdd = $user['created_at'];
         $cRequest = CRequest::find()->where(['user_id' => $user['id']])->one();
 
         if($cRequest && $cRequest['status'] !=  CRequest::STATUS_GENERATING || $cRequest['status'] !=  CRequest::STATUS_ACTIVE){
-          if((time() - $timeUserAdd) > 60){
-              Request::sendMessage(["text" => Config::get(Config::VAR_TEXT_NO_REQUESTS), "chat_id" => $chat_id, "reply_markup" => \Longman\TelegramBot\Entities\Keyboard::remove()]);
-
+          if((time() - $timeUserAdd) > 600){
+              Request::sendMessage(["text" => Config::get(Config::VAR_TEXT_NO_CREATE_REQUEST), "chat_id" => $chat_id, "reply_markup" => \Longman\TelegramBot\Entities\Keyboard::remove()]);
           }
             return true;
         }
