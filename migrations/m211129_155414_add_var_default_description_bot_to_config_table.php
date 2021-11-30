298c26ff1d800f26c01743b1a691d4aa872da924
vreds <slyvkinv@gmail.com>
2021-11-30 04:20:02 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ migrations/m211129_155414_add_var_default_description_bot_to_config_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m211129_155414_add_var_default_description_bot_to_config_table
+ */
+class m211129_155414_add_var_default_description_bot_to_config_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->insert(\app\models\Config::tableName(), [
+            "variable" => \app\models\Config::VAR_DEFAULT_LINK_TEXT,
+            "value" => "",
+            "comment" => "Приглашай друзей и получай бесплатные тесты! {link}"
+        ]);
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m211129_155414_add_var_default_description_bot_to_config_table cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m211129_155414_add_var_default_description_bot_to_config_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
---
+++ views/bot/_form.php
@@ -0,0 +1,49 @@
+<?php
+
+use app\models\Bot;
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Bot */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="bot-form">
+
+    <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
+
+    <?= $form->field($model, 'platform')->dropDownList(Bot::getPlatforms()) ?>
+    <?= $form->field($model, 'type')->dropDownList(Bot::getTypes()) ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'bot_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'token')->textarea(['rows' => 1]) ?>
+
+    <?= $form->field($model, 'free_requests')->textInput() ?>
+
+    <?= $form->field($model, 'requests_for_ref')->textInput() ?>
+
+    <?= $form->field($model, 'payment_system')->dropDownList(Bot::getPaymentSystems()) ?>
+
+	<?= $form->field($model, 'message_after_request_if_no_requests')->textarea(['rows' => 3]) ?>
+
+
+    <?= $form->field($model, 'custom_description')->textarea(['rows' => 3]) ?>
+
+    <?= $form->field($model, 'bot_image')->fileInput() ?>
+	<?= $form->field($model, 'reserve_bot')->textInput() ?>
+
+    <?php foreach (\app\helpers\CountryHelper::getCountries() as $k => $country) {
+        echo $form->field($model, 'country_'.$k)->checkbox(['label' => $country]);
+    }?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
