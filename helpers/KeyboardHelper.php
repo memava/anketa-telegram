e2a0da3192231a5358100846824afb196080e40c
eugenemihailov <95231149+eugenemihailov@users.noreply.github.com>
2021-12-01 18:50:41 +0200

    Update KeyboardHelper.php

--- helpers/KeyboardHelper.php
+++ helpers/KeyboardHelper.php
@@ -13,12 +13,14 @@ class KeyboardHelper
 	const BTN_CHANGE_COUNTRY = "Сменить страну";
 	const BTN_DONATE = "Поддержать бота / Donate";
 	const BTN_MAIN_MENU = "Главное меню";
+	const BTN_PAY = "Пополнить балланс";
 
 	const BUTTONS = [
 		self::BTN_MY_PROFILE,
 		self::BTN_CREATE_REQUEST,
 		self::BTN_CHANGE_COUNTRY,
 		self::BTN_DONATE,
+		self::BTN_PAY,
 		self::BTN_MAIN_MENU
 	];
 
