6fee376c496bbe39b51bbd86e85f94bff6fe21ea
memava <memava@asd.pl>
2021-12-20 16:03:26 +0200

    notifications

--- controllers/NotificationController.php
+++ controllers/NotificationController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 
 use app\models\Notification;
 use app\models\NotificationSearch;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -21,6 +22,15 @@ class NotificationController extends Controller
         return array_merge(
             parent::behaviors(),
             [
+                'access' => [
+                    'class' => AccessControl::className(),
+                    'rules' => [
+                        [
+                            'allow' => true,
+                            'roles' => ['@'],
+                        ]
+                    ],
+                ],
                 'verbs' => [
                     'class' => VerbFilter::className(),
                     'actions' => [
