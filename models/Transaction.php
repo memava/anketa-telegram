5062990b77d8d18dcf7fb7a7d44fa63fba786e6f
memava <memava@asd.pl>
2021-12-29 14:27:08 +0200

    разделение надписей

--- models/Transaction.php
+++ models/Transaction.php
@@ -244,7 +244,7 @@ class Transaction extends \yii\db\ActiveRecord
         ];
 
         $client = new Client();
-        $post = $client->post( "https://stage-papi.xpay.com.ua:488/xpay", ["body" => json_encode($data)]);
+        $post = $client->post( "https://papi.xpay.com.ua:488/xpay", ["body" => json_encode($data)]);
         $response = (string) $post->getBody();
         //return $response;
         return json_decode($response, true)["Data"]["URI"];
